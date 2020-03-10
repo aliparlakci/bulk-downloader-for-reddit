4b79491f67a81e1a40a1fc0950359d8d0228e975
wAuner <18383180+wAuner@users.noreply.github.com>
2020-03-10 09:53:49 +0100

    dockerize application

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:latest
+
+
+
+WORKDIR "/root/Bulk Downloader for Reddit"
+COPY ./requirements.txt ./
+RUN ["pip", "install", "-r", "requirements.txt"]
+EXPOSE 8080
+
+CMD ["/bin/bash"]  
--- src/searcher.py
+++ src/searcher.py
@@ -29,7 +29,7 @@ def beginPraw(config,user_agent = str(socket.gethostname())):
             """
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            server.bind(('localhost', self.PORT))
+            server.bind(('0.0.0.0', self.PORT))
             server.listen(1)
             client = server.accept()[0]
             server.close()
