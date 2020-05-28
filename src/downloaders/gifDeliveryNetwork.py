17d454c1ba291a84895808ada7e3949e05fac591
Ali <parlakciali@gmail.com>
2020-05-28 20:31:48 +0300

    Initial commit

--- src/downloaders/gifDeliveryNetwork.py
+++ src/downloaders/gifDeliveryNetwork.py
@@ -9,7 +9,7 @@ from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely,
 from src.utils import nameCorrector
 from src.utils import printToFile as print
 
-class Redgifs:
+class GifDeliveryNetwork:
     def __init__(self,directory,POST):
         try:
             POST['mediaURL'] = self.getLink(POST['postURL'])
@@ -17,7 +17,7 @@ class Redgifs:
             raise NotADownloadableLinkError("Could not read the page source")
 
         POST['postExt'] = getExtension(POST['mediaURL'])
-
+        
         if not os.path.exists(directory): os.makedirs(directory)
         title = nameCorrector(POST['postTitle'])
 
@@ -31,7 +31,7 @@ class Redgifs:
         tempDir = directory / (
             POST["postSubmitter"]+"_"+title+"_"+POST['postId']+".tmp"
         )
-
+        
         try:
             getFile(fileDir,tempDir,POST['mediaURL'])
         except FileNameTooLong:
@@ -39,27 +39,27 @@ class Redgifs:
             tempDir = directory / (POST['postId']+".tmp")
 
             getFile(fileDir,tempDir,POST['mediaURL'])
-
+      
     def getLink(self, url):
         """Extract direct link to the video from page's source
         and return it
         """
 
-        if '.webm' in url or '.mp4' in url or '.gif' in url:
+        if '.webm' in url.split('/')[-1] or '.mp4' in url.split('/')[-1] or '.gif' in url.split('/')[-1]:
             return url
 
         if url[-1:] == '/':
             url = url[:-1]
 
-        url = "https://redgifs.com/watch/" + url.split('/')[-1]
+        url = "https://www.gifdeliverynetwork.com/" + url.split('/')[-1]
 
         pageSource = (urllib.request.urlopen(url).read().decode())
 
         soup = BeautifulSoup(pageSource, "html.parser")
-        attributes = {"data-react-helmet":"true","type":"application/ld+json"}
-        content = soup.find("script",attrs=attributes)
+        attributes = {"id":"mp4Source","type":"video/mp4"}
+        content = soup.find("source",attrs=attributes)
 
         if content is None:
             raise NotADownloadableLinkError("Could not read the page source")
 
-        return json.loads(content.contents[0])["video"]["contentUrl"]
+        return content["src"]
