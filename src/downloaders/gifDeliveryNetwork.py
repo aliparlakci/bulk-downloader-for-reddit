9bc324538cbca5909cbff2b2f7980002476d469a
Serene-Arc <serenical@gmail.com>
2021-02-06 13:21:16 +1000

    Format file to PEP8

--- src/downloaders/gifDeliveryNetwork.py
+++ src/downloaders/gifDeliveryNetwork.py
@@ -1,30 +1,31 @@
-import json
 import os
 import urllib.request
 from bs4 import BeautifulSoup
 
 from src.downloaders.downloaderUtils import getFile, getExtension
-from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely, 
+from src.errors import (FileNameTooLong, AlbumNotDownloadedCompletely,
                         NotADownloadableLinkError, FileAlreadyExistsError)
 from src.utils import GLOBAL
 from src.utils import printToFile as print
 
+
 class GifDeliveryNetwork:
-    def __init__(self,directory,POST):
+    def __init__(self, directory, post):
         try:
-            POST['MEDIAURL'] = self.getLink(POST['CONTENTURL'])
+            post['MEDIAURL'] = self.getLink(post['CONTENTURL'])
         except IndexError:
             raise NotADownloadableLinkError("Could not read the page source")
 
-        POST['EXTENSION'] = getExtension(POST['MEDIAURL'])
-        
-        if not os.path.exists(directory): os.makedirs(directory)
+        post['EXTENSION'] = getExtension(post['MEDIAURL'])
 
-        filename = GLOBAL.config['filename'].format(**POST)+POST["EXTENSION"]
-        shortFilename = POST['POSTID']+POST['EXTENSION']
-        
-        getFile(filename,shortFilename,directory,POST['MEDIAURL'])
-    
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        filename = GLOBAL.config['filename'].format(**post) + post["EXTENSION"]
+        short_filename = post['POSTID'] + post['EXTENSION']
+
+        getFile(filename, short_filename, directory, post['MEDIAURL'])
+
     @staticmethod
     def getLink(url):
         """Extract direct link to the video from page's source
@@ -39,14 +40,13 @@ class GifDeliveryNetwork:
 
         url = "https://www.gifdeliverynetwork.com/" + url.split('/')[-1]
 
-        pageSource = (urllib.request.urlopen(url).read().decode())
+        page_source = (urllib.request.urlopen(url).read().decode())
 
-        soup = BeautifulSoup(pageSource, "html.parser")
-        attributes = {"id":"mp4Source","type":"video/mp4"}
-        content = soup.find("source",attrs=attributes)
+        soup = BeautifulSoup(page_source, "html.parser")
+        attributes = {"id": "mp4Source", "type": "video/mp4"}
+        content = soup.find("source", attrs=attributes)
 
         if content is None:
-            
             raise NotADownloadableLinkError("Could not read the page source")
 
         return content["src"]
